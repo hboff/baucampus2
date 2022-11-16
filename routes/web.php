5aa1a8aaefd5171d981625ae5a38d73348cf0891
Oskar Lohse <oskar@baucampus.de>
2022-11-16 15:15:29 +0100

    baubegleitung

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/baubegleitung', function() {
+    return view('unterseiten/baubegleitung');
+});
