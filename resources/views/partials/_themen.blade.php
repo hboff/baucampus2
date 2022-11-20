af31a2851d1c36586da91ee45d7b69887268b795
Oskar Lohse <oskar@baucampus.de>
2022-11-20 16:29:18 +0100

    idee

--- resources/views/partials/_themen.blade.php
+++ resources/views/partials/_themen.blade.php
@@ -1,13 +1,25 @@
-
-@section('ortsname')
 @php
+$x[]=$ort['ort'];
+$y[]=$ort['laengengrad'];
+$z[]=$ort['breitengrad'];
+
+$hauptort= $ort['ort'][0]; 
+$ostwert= $y['laengengrad'][0];
+$nordwert = $ort['breitengrad'][3];
 $i=0;
 @endphp
+
+@foreach($ort as $key => $val)
+  $ost = $ostwert-$val[2];
+  $nord = $nordwert-$val[3];
+  $entfernung["$key"] = $ost*$ost+$nord*$nord;
+@endforeach
+@asort($entfernung)
+@section('ortsname')
 @foreach($ort as $orte)
-@if ($i++ > 50)
+@if ($i++ > 10)
 @else
-<a href="/{{$orte->ort}}">Baugutachter {{$orte['ort']}}</a><br>
+{{$y[0]}} {{$z}}
 @endif
 @endforeach
 @endsection
-
