c0e00a83fa932ce06f748da868803e2613629961
hboff <oskar@baucampus.de>
2023-02-05 17:10:40 +0100

    1

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,9 @@
 <?php
 
-use App\Models\Orteat;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\OrteController;
-use App\Http\Controllers\OrteatController;
-use App\Http\Controllers\GutachterController;
 use App\Http\Controllers\ContactController;
-use App\Models\Ort;
-use App\Models\Gutachter;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+use App\Http\Controllers\OrteatController;
+use App\Http\Controllers\OrteController;
+use Illuminate\Support\Facades\Route;
 
 $routes = [
     '/',
@@ -31,7 +15,8 @@ $routes = [
     '/immobilienbewertung',
 ];
 
-$domains = [    'immobilienbewertung-bielefeld.com' => [        'laengengrad' => [51.0, 52.0],
+$domains = [    
+    'immobilienbewertung-bielefeld.com' => [        'laengengrad' => [51.0, 52.0],
         'breitengrad' => [7.0, 8.0],
     ],
     'immobilienbewertung-wuppertal.eu' => [        'laengengrad' => [51.0, 52.0],
@@ -63,21 +48,6 @@ foreach ($domains as $domain => $values) {
         Route::get('/orte/{ort}', 'OrteController@show')->name('orte.show');
     });
 }
-///////Route::group(['domain' => 'baucampus.at'], function () {
-///////Route::get('/startseite', [OrteatController::class, 'startseite']);
-///////Route::get('/gutachter', [GutachterController::class, 'index']);
-///////Route::get('/{ort}/bausachverstaendiger', [OrteatController::class, 'show'], function (Request $request){});
-///////Route::get('/gutachter/{gutachter}', [GutachterController::class, 'show'], function (Request $request){});
-///////Route::get('/schimmelpilz', [OrteatController::class, 'schimmelpilz']);
-///////Route::get('/hauskaufberatung', [OrteatController::class, 'hauskaufberatung']);
-///////Route::get('/baubegleitung', [OrteatController::class, 'baubegleitung']);
-///////Route::get('/bauschaden', [OrteatController::class, 'bauschaden']);
-///////Route::get('/energieberatung', [OrteatController::class, 'energieberatung']);
-///////Route::get('/immobilienbewertung', [OrteatController::class, 'immobilienbewertung']);
-///////Route::get('/', [OrteatController::class, 'index']);
-///////Route::get('/team', [OrteatController::class, 'team']);
-///////Route::get('/gutachter', [OrteatController::class, 'gutachter']);
-///////});
 //   Route::get('/gutachter', [GutachterController::class, 'index']);
 //Route::get('/kontakt', function(){
 //    return view('unterseiten/kontakt');
