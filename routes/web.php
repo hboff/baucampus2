7f97d9324c1d6e1dc27a99c8ba3a070e9676fd41
hboff <oskar@baucampus.de>
2023-02-05 17:25:31 +0100

    1

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,37 @@ $routes = [
     '/immobilienbewertung',
 ];
 
-$domains = [
-    'immobilienbewertung-bielefeld.com',
-    'immobilienbewertung-wuppertal.eu',
-    'baucampus.at',
-    'baucampus.be',
-    'baucampus.nl',
+$domains = [    
+    'immobilienbewertung-bielefeld.com' => [        'laengengrad' => [51.0, 52.0],
+        'breitengrad' => [7.0, 8.0],
+    ],
+    'immobilienbewertung-wuppertal.eu' => [        'laengengrad' => [51.0, 52.0],
+        'breitengrad' => [7.0, 8.0],
+    ],
+    'baucampus.at' => [        'laengengrad' => [48.0, 49.0],
+        'breitengrad' => [12.0, 13.0],
+    ],
+    'baucampus.be' => [        'laengengrad' => [50.0, 51.0],
+        'breitengrad' => [3.0, 4.0],
+    ],
+    'baucampus.nl' => [        'laengengrad' => [52.0, 53.0],
+        'breitengrad' => [4.0, 5.0],
+    ],
 ];
 
-foreach ($domains as $domain) {
-    Route::domain($domain)->group(function () use ($routes) {
-        foreach ($routes as $route) {
-            $controllerMethod = str_replace('/', '', $route);
-            Route::get($route, [OrteatController::class, $controllerMethod]);
-        }
+foreach ($domains as $domain => $values) {
+    Route::domain($domain)->group(function () use ($values) {
+        Route::get('/', 'OrteController@index')->name('index');
+        Route::get('/startseite', 'OrteController@startseite')->name('startseite');
+        Route::get('/schimmelpilz', 'OrteController@schimmelpilz')->name('schimmelpilz');
+        Route::get('/hauskaufberatung', 'OrteController@hauskaufberatung')->name('hauskaufberatung');
+        Route::get('/baubegleitung', 'OrteController@baubegleitung')->name('baubegleitung');
+        Route::get('/bauschaden', 'OrteController@bauschaden')->name('bauschaden');
+        Route::get('/energieberatung', 'OrteController@energieberatung')->name('energieberatung');
+        Route::get('/immobilienbewertung', 'OrteController@immobilienbewertung')->name('immobilienbewertung');
+        Route::get('/team', 'OrteController@team')->name('team');
+        Route::get('/gutachter', 'OrteController@gutachter')->name('gutachter');
+        Route::get('/orte/{ort}', 'OrteController@show')->name('orte.show');
     });
 }
 //   Route::get('/gutachter', [GutachterController::class, 'index']);
