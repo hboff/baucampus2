c95545b650fe9e40bdcb880182e647b74b30942d
hboff <oskar@baucampus.de>
2023-02-06 00:32:47 +0100

    7

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+
+use App\Http\Controllers\OrteatController;
 use Illuminate\Support\Facades\DB;
+
 
 /*
 |--------------------------------------------------------------------------
