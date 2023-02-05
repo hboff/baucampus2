535fd7fa0b6f752d8eb169ba207e5e1a9c81265d
hboff <oskar@baucampus.de>
2023-02-05 17:26:59 +0100

    working solution

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,25 @@
 <?php
 
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\OrteatController;
-use App\Http\Controllers\OrteController;
+use App\Models\Orteat;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrteController;
+use App\Http\Controllers\OrteatController;
+use App\Http\Controllers\GutachterController;
+use App\Http\Controllers\ContactController;
+use App\Models\Ort;
+use App\Models\Gutachter;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 
 $routes = [
     '/',
@@ -15,39 +31,41 @@ $routes = [
     '/immobilienbewertung',
 ];
 
-$domains = [    
-    'immobilienbewertung-bielefeld.com' => [        'laengengrad' => [51.0, 52.0],
-        'breitengrad' => [7.0, 8.0],
-    ],
-    'immobilienbewertung-wuppertal.eu' => [        'laengengrad' => [51.0, 52.0],
-        'breitengrad' => [7.0, 8.0],
-    ],
-    'baucampus.at' => [        'laengengrad' => [48.0, 49.0],
-        'breitengrad' => [12.0, 13.0],
-    ],
-    'baucampus.be' => [        'laengengrad' => [50.0, 51.0],
-        'breitengrad' => [3.0, 4.0],
-    ],
-    'baucampus.nl' => [        'laengengrad' => [52.0, 53.0],
-        'breitengrad' => [4.0, 5.0],
-    ],
+$domains = [
+    'immobilienbewertung-bielefeld.com',
+    'immobilienbewertung-wuppertal.eu',
+    'baucampus.at',
+    'baucampus.be',
+    'baucampus.nl',
 ];
 
-foreach ($domains as $domain => $values) {
-    Route::domain($domain)->group(function () use ($values) {
-        Route::get('/', 'OrteController@index')->name('index');
-        Route::get('/startseite', 'OrteController@startseite')->name('startseite');
-        Route::get('/schimmelpilz', 'OrteController@schimmelpilz')->name('schimmelpilz');
-        Route::get('/hauskaufberatung', 'OrteController@hauskaufberatung')->name('hauskaufberatung');
-        Route::get('/baubegleitung', 'OrteController@baubegleitung')->name('baubegleitung');
-        Route::get('/bauschaden', 'OrteController@bauschaden')->name('bauschaden');
-        Route::get('/energieberatung', 'OrteController@energieberatung')->name('energieberatung');
-        Route::get('/immobilienbewertung', 'OrteController@immobilienbewertung')->name('immobilienbewertung');
-        Route::get('/team', 'OrteController@team')->name('team');
-        Route::get('/gutachter', 'OrteController@gutachter')->name('gutachter');
-        Route::get('/orte/{ort}', 'OrteController@show')->name('orte.show');
+foreach ($domains as $domain) {
+    Route::domain($domain)->group(function () use ($routes) {
+        foreach ($routes as $route) {
+            $controllerMethod = str_replace('/', '', $route);
+            Route::get($route, [OrteatController::class, $controllerMethod]);
+        }
     });
 }
+
+
+//Route::group(['domain' => 'baucampus.at'], function () {
+//Route::get('/startseite', [OrteatController::class, 'startseite']);
+//Route::get('/gutachter', [GutachterController::class, 'index']);
+//Route::get('/{ort}/bausachverstaendiger', [OrteatController::class, 'show'], function (Request $request){});
+//Route::get('/gutachter/{gutachter}', [GutachterController::class, 'show'], function (Request $request){});
+//Route::get('/schimmelpilz', [OrteatController::class, 'schimmelpilz']);
+//Route::get('/hauskaufberatung', [OrteatController::class, 'hauskaufberatung']);
+//Route::get('/baubegleitung', [OrteatController::class, 'baubegleitung']);
+//Route::get('/bauschaden', [OrteatController::class, 'bauschaden']);
+//Route::get('/energieberatung', [OrteatController::class, 'energieberatung']);
+//Route::get('/immobilienbewertung', [OrteatController::class, 'immobilienbewertung']);
+//Route::get('/', [OrteatController::class, 'index']);
+//Route::get('/team', [OrteatController::class, 'team']);
+//Route::get('/gutachter', [OrteatController::class, 'gutachter']);
+//});
+
+
 //   Route::get('/gutachter', [GutachterController::class, 'index']);
 //Route::get('/kontakt', function(){
 //    return view('unterseiten/kontakt');
