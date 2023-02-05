b8b4139b5ca8262ee7a3d09d6a222f4e9c82fd5b
hboff <oskar@baucampus.de>
2023-02-05 17:02:59 +0100

    at und orte getauscht

--- app/Http/Controllers/OrteController.php
+++ app/Http/Controllers/OrteController.php
@@ -4,58 +4,56 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Ort;
-
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\DB;
 
-class OrteController extends Controller
+class OrteatController extends Controller
 {
     // Show single lisitng
-    public function show($ort) {
-        $status='ch';
-        return view('unterseiten.bausachverstaendiger', compact('status'), [
-            'ortsname'=> $ort,
-            ]);    }          
-    
+    public function show($ortat) {
+        $status='at';
+        return view('unterseiten.bausachverstaendiger', [
+            'ortsname'=> $ortat,
+            ]);    }         
     public function index() {
-        $status='ch';
+        $status='at';
         return view ('index', compact('status'));
     }
     public function startseite() {
-        $status='ch';
+        $status='at';
         return view ('startseite', compact('status'));
     }
     public function schimmelpilz() {
-        $status='ch';
+        $status='at';
         return view ('unterseiten.schimmelpilz', compact('status'));
     }
     public function hauskaufberatung() {
-        $status='ch';
+        $status='at';
         return view ('unterseiten.hauskaufberatung', compact('status'));
     }
     public function baubegleitung() {
-        $status='ch';
+        $status='at';
         return view ('unterseiten.baubegleitung', compact('status'));
     }
     public function bauschaden() {
-        $status='ch';
+        $status='at';
         return view ('unterseiten.bauschaden', compact('status'));
     }
     public function energieberatung() {
-        $status='ch';
+        $status='at';
         return view ('unterseiten.energieberatung', compact('status'));
     }
     public function immobilienbewertung() {
-        $status='ch';
+        $status='at';
         return view ('unterseiten.immobilienbewertung', compact('status'));
     }
     public function team() {
-        $status='ch';
+        $status='at';
         return view ('unterseiten.team', compact('status'));
     }
     public function gutachter() {
-        $status='ch';
+        $status='at';
         return view ('unterseiten.gutachter', compact('status'));
     }
 }
