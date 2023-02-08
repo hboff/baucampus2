d9193378276fefee53dfd528b96d88cfd995d92c
hboff <oskar@baucampus.de>
2023-02-08 13:06:56 +0100

    3

--- resources/views/gutachter.blade.php
+++ resources/views/gutachter.blade.php
@@ -148,11 +148,11 @@ p + p {
 
 </table>
 
-<div id="map" style="width:100%; height:400px;"></div>
 
 </div><!-- End About Section -->
 <!-- END BLOG ENTRIES -->
 </div>
+<div id="map" style="width:100%; height:400px;"></div>
 
 <!-- END GRID -->
 </div><br>
