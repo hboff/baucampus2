377cd12e74206a24f68f2a2ee6a5de5aa4cecec1
Oskar Lohse <oskar@baucampus.de>
2023-02-12 18:57:12 +0100

    sidebar test 15

--- resources/views/partials/_sidebar.blade.php
+++ resources/views/partials/_sidebar.blade.php
@@ -4,19 +4,18 @@
 <!-- ======= Header ======= -->
 <div class="d-flex flex-column" id="sidebarprofile">
   <!-- ======= profile1 ======= -->
-@foreach ($expert as $row)
-@if ($ortsname == $row['ort'])
+
   <div class="profile">
-    <img src="/img/{{$row['Photo']}}" alt="" class="mt-4" style="    vertical-align: middle;
+    <img src="" alt="" class="mt-4" style="    vertical-align: middle;
 width: 180px;
 height: 180px;
 border-radius: 50%;
 object-fit: cover;
 object-position: center;">
 
-    <h1 class="text-light mb-3"><a href="/gutachter/{{$row->LastName}}">{{$row['FirstName']}} {{$row['LastName']}}</a></h1>
-    <p class="text-light text-center mb-0">{{$row['Phone']}}</p>
-    <p class="text-light text-center">{{$row['Email']}}</p>
+    <h1 class="text-light mb-3"><a href="">1</a></h1>
+    <p class="text-light text-center mb-0">2</p>
+    <p class="text-light text-center">3</p>
   </div>
 @else
 @endif
