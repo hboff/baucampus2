c4a8194d7e54d22604b6bb3c5f843244a91a1eb9
Oskar Lohse <oskar@baucampus.de>
2023-02-12 17:37:36 +0100

    sidebar test 6

--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,7 @@ foreach ($domains as $domain => $domainData) {
 Route::domain($domain)->group(function () use ($routes, $domainData) {
     Route::get('/', [OrteatController::class, 'index']);
     Route::get('/gutachter/{gutachter}', [GutachterController::class, 'show'], function (Request $request){});
-    Route::get('/{ort}/bausachverstaendiger', [OrteatController::class, 'show'], function (Request $request){});
+    //Route::get('/{ort}/bausachverstaendiger', [OrteatController::class, 'show'], function (Request $request){});
     Route::get('contact-us', [ContactController::class, 'index']);
     Route::post('contact-us', [ContactController::class, 'store'])->name('contact.us.store');
 
@@ -95,6 +95,7 @@ Route::domain($domain)->group(function () use ($routes, $domainData) {
 });
 }
 });
+Route::get('/{ort}/bausachverstaendiger', [OrteatController::class, 'show'], function (Request $request) use($expert){});
 }
 
 
