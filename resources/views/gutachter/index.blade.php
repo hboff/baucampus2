00da4a54a8307717454de5b76a1adb4fa57b0d15
Oskar Lohse <oskar@baucampus.de>
2023-02-12 15:12:52 +0100

    test

--- resources/views/gutachter/index.blade.php
+++ resources/views/gutachter/index.blade.php
@@ -114,41 +114,7 @@ $contact=1;
       </div>
     </div>
   </nav>
-  <header id="header">
 
-<!-- ======= Header ======= -->
-<div class="d-flex flex-column" id="sidebarprofile">
-  <!-- ======= profile1 ======= -->
-@foreach ($expert as $row)
-@if ($ortsname == $row['ort'])
-  <div class="profile">
-    <img src="/img/{{$row['Photo']}}" alt="" class="mt-4" style="    vertical-align: middle;
-width: 180px;
-height: 180px;
-border-radius: 50%;
-object-fit: cover;
-object-position: center;">
-
-    <h1 class="text-light mb-3"><a href="/gutachter/{{$row->LastName}}">{{$row['FirstName']}} {{$row['LastName']}}</a></h1>
-    <p class="text-light text-center mb-0">{{$row['Phone']}}</p>
-    <p class="text-light text-center">{{$row['Email']}}</p>
-  </div>
-@else
-@endif
-@endforeach
-  <div class="profile">
-    <img src="/img/logo.png" alt="" class="mt-2" style="    vertical-align: middle;
-width: 120px;
-height: 120px;
-border-style:none;
-object-fit: cover;
-object-position: center;">
-    <h1 class="text-light mb-3"><a href="index.html">Hauptstelle Baucampus</a></h1>
-    <p class="text-light text-center mb-0">Telefon: 05722 913800</p>
-    <p class="text-light text-center">kontakt@baucampus.de</p>
-  </div>
-</div>
-</header><!-- End Header -->
 @foreach ($gutachter as $gutachters)
 @if ($name == $gutachters['UrlName'])
 <!-- ======= Hero Section ======= -->
