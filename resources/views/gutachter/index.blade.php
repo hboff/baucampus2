a5717a99cef5a689ea337606361de40a0ab1f21b
Oskar Lohse <oskar@baucampus.de>
2023-02-12 15:18:40 +0100

    test 2

--- resources/views/gutachter/index.blade.php
+++ resources/views/gutachter/index.blade.php
@@ -146,10 +146,10 @@ $contact=1;
         </div>
 
         <div class="row">
-          <div class="col-lg-4" data-aos="fade-right">
+          <div class="col-lg-6" data-aos="fade-right">
             <img src="/img/{{$gutachters['Photo']}}" class="img-fluid" alt="">
           </div>
-          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
             <h3>Unternehmer &amp; Bausachverständiger</h3>
             <p class="fst-italic">
               Telefon: {{$gutachters['Phone']}}
