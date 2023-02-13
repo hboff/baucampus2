6d058e0f592dae6a7e42b8d1a898edcd4455f971
Oskar Lohse <oskar@baucampus.de>
2023-02-13 09:14:49 +0100

    test

--- resources/views/hauskaufberatung.blade.php
+++ resources/views/hauskaufberatung.blade.php
@@ -22,6 +22,23 @@
         <div class="section-title">
           <h2>Hauskaufberatung</h2>
         </div>
+        @foreach ($expert as $row)
+@if ($ortsname == $row['ort'])
+  <div class="profile">
+    <img src="/img/{{$row['Photo']}}" alt="" class="mt-4" style="    vertical-align: middle;
+width: 180px;
+height: 180px;
+border-radius: 50%;
+object-fit: cover;
+object-position: center;">
+
+    <h1 class="text-light mb-3"><a href="/gutachter/{{$row->LastName}}">{{$row['FirstName']}} {{$row['LastName']}}</a></h1>
+    <p class="text-light text-center mb-0">{{$row['Phone']}}</p>
+    <p class="text-light text-center">{{$row['Email']}}</p>
+  </div>
+@else
+@endif
+@endforeach
 <table>
     <thead>
         <tr>
--- resources/views/unterseiten/bausachverstaendiger.blade.php
+++ resources/views/unterseiten/bausachverstaendiger.blade.php
@@ -3,31 +3,11 @@
 
 <title>Bausachverstädiger {{$ortsname}}</title>
 <meta name="Description" content="Unsere Leistungen in {{$ortsname}}.">
-@isset($expert)
-@include('partials._sidebar')
-@endisset
+@include('partials._sidebar', ['expert' => $expert])
 <main id="main">
 </br>
 </br>
-@isset($expert)
-@foreach ($expert as $row)
-@if ($ortsname == $row['ort'])
-  <div class="profile">
-    <img src="/img/{{$row['Photo']}}" alt="" class="mt-4" style="    vertical-align: middle;
-width: 180px;
-height: 180px;
-border-radius: 50%;
-object-fit: cover;
-object-position: center;">
 
-    <h1 class="text-light mb-3"><a href="/gutachter/{{$row->LastName}}">{{$row['FirstName']}} {{$row['LastName']}}</a></h1>
-    <p class="text-light text-center mb-0">{{$row['Phone']}}</p>
-    <p class="text-light text-center">{{$row['Email']}}</p>
-  </div>
-@else
-@endif
-@endforeach
-@endisset
 <div id="about" class="about pt-5">
       <div class="container">
 
