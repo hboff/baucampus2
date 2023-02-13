398c96b39862f99d623d6c7a12d7f5dda8e0f374
Oskar Lohse <oskar@baucampus.de>
2023-02-13 16:27:44 +0100

    nearesCity test 16

--- resources/views/partials/_1.blade.php
+++ resources/views/partials/_1.blade.php
@@ -1,5 +1,5 @@
 @isset ($nearestCities)
-{{@foreach ($nearestCities as $city)
+@foreach ($nearestCities as $city)
 
 <a href="/{{$city->city}}/bausachverstaendiger">Baugutachter {{$city->city}}</a>
 
