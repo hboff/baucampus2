9f5f56cb25b62814569abafc81c65ac962571c3e
Oskar Lohse <oskar@baucampus.de>
2023-02-13 11:23:18 +0100

    index scroll button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,13 +7,13 @@
 <img sizes="(max-width: 1400px) 100vw, 1400px"  src="/img/baucampus.jpg" alt="Baucampus" style="width:100%;min-height:350px;max-height:100vh; object-fit: cover; object-position:center;">
   
   <div id="section03" class="demo">
-  <a href="#about"><span></span>Scroll</a>
+  <a href="1"><span></span>Scroll</a>
 </div>
 
 </div>
 
-<section id="1">
-<div class="w3-row-padding" style="margin:10vh; background-color:#fff;"> 
+
+<div id="1" class="w3-row-padding" style="margin:10vh; background-color:#fff;"> 
 		<h2 class="heading-separator w3-center">Baucampus</h2>
                 <p class="subheading-text w3-center">Das Sachverständigennetzwerk</p>
 </div>
@@ -69,6 +69,6 @@
 </div>
 <br/>
 </div>
-</section>
+
 @endsection
 
