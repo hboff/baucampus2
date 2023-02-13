d99892efa4e23cdc3193c12ffb25a2852a97d66e
Oskar Lohse <oskar@baucampus.de>
2023-02-13 12:21:37 +0100

    gutachter design fix

--- resources/views/gutachter.blade.php
+++ resources/views/gutachter.blade.php
@@ -139,7 +139,8 @@ p + p {
   @foreach($gutachter as $gutachters)
   
   <tr>
-    <td><a  href="/gutachter/{{$gutachters['UrlName']}}" target="_blank"><img src="/img/{{$gutachters['Photo']}}" style="vertical-align: middle; width: 100px; height: 100px; border-radius: 50%; object-fit: cover; object-position: center;">{{$gutachters['FirstName']}} {{$gutachters['LastName']}}</a></td>
+    <td><a  href="/gutachter/{{$gutachters['UrlName']}}" target="_blank"><img src="/img/{{$gutachters['Photo']}}" style="vertical-align: middle; width: 100px; height: 100px; border-radius: 50%; object-fit: cover; object-position: center;">}</a></td>
+    <td><a  href="/gutachter/{{$gutachters['UrlName']}}" class="link-dark" target="_blank">{{$gutachters['FirstName']}} {{$gutachters['LastName']}}</a></td>
     <td>{{$gutachters['Adress']}}<br/>{{$gutachters['PostalCode']}} {{$gutachters['City']}}</td>
     <td>{{$gutachters['Phone']}} <br/>{{$gutachters['Email']}}<br/><a  href="{{$gutachters['Website']}}" target="_blank">{{$gutachters['Website']}}</a></td>
 
