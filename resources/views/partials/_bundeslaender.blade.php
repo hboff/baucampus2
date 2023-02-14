8b13d3419146208f98afaff56c1c9718f534c17c
Oskar Lohse <oskar@baucampus.de>
2023-02-14 12:16:27 +0100

    interseiten region 2

--- resources/views/partials/_bundeslaender.blade.php
+++ resources/views/partials/_bundeslaender.blade.php
@@ -2,12 +2,12 @@
     @foreach($regions as $region)
     @if(str_contains(url()->current(), "baucampus.de"))
     @if ($region->Country == 'Germany')
-    <a class="link-dark" href="/bausachverstaendiger/{{$region->Region}}">Baugutachter {{$region->Region}}</a><br>
+    <a class="link-dark" href="/baugutachter/{{$region->Region}}">Baugutachter {{$region->Region}}</a><br>
     @endif
 @endif
 @if(str_contains(url()->current(), "baucampus.be"))
 @if ($region->Country == 'Austria')
-    <a class="link-dark" href="/bausachverstaendiger/{{$region->Region}}">Baugutachter {{$region->Region}}</a><br>
+    <a class="link-dark" href="/baugutachter/{{$region->Region}}">Baugutachter {{$region->Region}}</a><br>
     @endif
     @endif
     @endforeach
