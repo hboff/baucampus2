68104503e27c44a80f62846a20303119a01800d1
Oskar Lohse <oskar@baucampus.de>
2023-02-14 12:07:05 +0100

    unterseiten region

--- routes/web.php
+++ routes/web.php
@@ -66,6 +66,9 @@ Route::domain($domain)->group(function () use ($routes, $domainData) {
     Route::get('/', [OrteatController::class, 'index']);
     Route::get('/gutachter/{gutachter}', [GutachterController::class, 'show'], function (Request $request){});
     Route::get('/{ort}/bausachverstaendiger', [OrteatController::class, 'show'], function () use ($domainData) {});
+    Route::get('/{region}/bausachverstaendiger', function($region){
+        return view ('unterseiten.bausachverstaendiger', ['ortsname' => $region]);
+});
     Route::get('contact-us', [ContactController::class, 'index']);
     Route::post('contact-us', [ContactController::class, 'store'])->name('contact.us.store');
 foreach ($routes as $route) {
