ba423bed7fdc16cd6cba0df1679d5eeb05efe6a9
Oskar Lohse <oskar@baucampus.de>
2023-02-14 10:26:49 +0100

    test

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -344,7 +344,7 @@
     @yield('content')
     
     @include('partials._contact')
-    @if(str_contains(url()->current(), "{{$ort->ort}}"))
+    @if(str_contains(url()->current(), "{{$ortsname}}"))
     <div id="services" class="services">
         <div class="container">
 
