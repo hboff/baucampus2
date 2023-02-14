c2e78c7d40bb6dca42a6aac8d9d16cb04ee8ed3c
Oskar Lohse <oskar@baucampus.de>
2023-02-14 20:02:28 +0100

    sidebar test

--- resources/views/unterseiten/bausachverstaendiger.blade.php
+++ resources/views/unterseiten/bausachverstaendiger.blade.php
@@ -7,6 +7,7 @@ $footer=1;
 <meta name="Description" content="Unsere Leistungen in {{$ortsname}}.">
 @endsection
 @section('content')
+@if(str_contains(url()->current(), "bausachverstaendiger"))
 @include('partials._sidebar')
 
 <main id="main">
@@ -149,5 +150,147 @@ $footer=1;
 
 <!-- END GRID -->
 </div><br>
+@else
+<main id="main">
+</br>
+</br>
+
+<div id="about" class="about pt-5">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Hauskaufberatung {{$ortsname}}</h2>
+        </div>
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-right">
+          <p>Beim <b>Kauf eines Hauses</b> oder einer Eigentumswohnung stellen sich viele Fragen. Mit einer professionellen und unabhängigen <b>Hauskaufberatung</b> durch unsere Bausachverständigen gehen Sie den richtigen Schritt, um ein Haus oder eine Eigentumswohnung auf der Basis klarer <b>Fakten</b> zu erwerben.</p>
+
+<p>Im Vorfeld der Hauskaufberatung prüft ihr Bausachverständiger die <b>vorhandenen Unterlagen</b>, meist das Exposé und bereitet den Termin anhand der Eckdaten des Gebäudes durch eine <b>Marktwerteinschätzung</b> vor. Vor Ort begeht der Sachverständige mit Ihnen gemeinsam die Immobilie, die Sie erwerben möchten und stellt den <b>baulichen Zustand</b> und den eventuellen <b>Sanierungsbedarf</b> des Objektes fest. Zudem beantwortet der Baugutachter Fragestellungen, auf die es Ihnen besonders ankommt, z.B. bzgl. eines Umbaus.</p>
+<p>Im Anschluss an die Besichtigung der Immobilie findet eine <b>abschließende Besprechung</b> statt, bei der Ihnen ihr Bausachverständiger noch einmal alle Fakten, die Sie zum Kauf der Immobilie wissen müssen, verständlich und klar nachvollziehbar erklärt. Die Beratung durch unsere Bausachverständigen beim Kauf eines Hauses oder einer Eigentumswohnung gibt ihnen <b>Sicherheit</b>, so dass Sie ihre Kaufentscheidung beruhigt treffen können.</p>
+
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <img src="/img/hauskaufberatung-brochure.jpg" class="img-fluid" alt="">
+          </div>
+        </div>
+
+      </div>
+</div><!-- End About Section -->
+<!-- END BLOG ENTRIES -->
+</div>
+
+<!-- END GRID -->
+</div><br>
+    <!-- ======= About Section ======= -->
+    <div id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Baubegleitung {{$ortsname}}</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-right">
+          <p>Die <b>baubegleitende Qualitätssicherung</b> durch einen Bausachverständigen ermöglicht dem Bauherren eine neutrale Qualitätskontrolle für den Bau oder den Umbau ihrer Immobilie. Durch die steigende Konkurrenz und Kostendruck in der Baubranche leidet die Bauqualität. Hier kann ein Bausachverständiger helfend eingreifen und so Geld und Nerven sparen. Ein Baugutachter, der die <b>baubegleitende Qualitätssicherung</b> durchführt, stellt sicher, dass Baubeschreibung, Bauplanung und Bauausführung übereinstimmen.</p>
+<p> Ein Bausachverständiger sollte schon bereits vor dem Unterzeichnen der Bauunterlagen hinzugezogen werden, um mögliche <b>Planungsfehler zu vermeiden</b>. Hierzu bieten wir in unserem Ersttermin die Prüfung der Unterlagen auf Plausibilität und Vollständigkeit und bieten eine Überprüfung der Bauunterlagen in fachtechnischer und qualitätstechnischer Hinsicht an. Zu einem vollständigen Ersttermin im Rahmen der qualitätssicherenden Baubegleitung gehört auch die Besichtigung des Grundstücks.</p>
+<p>Nach dem eigentlichen Baubeginn werden durch den Bausachverständigen <b>einzelne Routinetermine</b>, also Baustellentermine, wahrgenommen, die nach der Fertigstellung einzelner Bauabschnitte erfolgen Hierzu gehören z.B. Fertigstellung des Kellers inkl. Abdichtung, nach Rohbau und Dachstuhl oder vor dem Innenputz nach erfolgter Elektro- und Sanitärinstallation. Kurz gesagt alle Arbeiten, die durch eines der nachfolgenden Gewerke verdeckt werden, werden im Verlauf der Baubegleitung durch den Baugutachter in <b>Augenschein genommen</b>.</p>
+<p>Am Ende der Bauphase steht die <b>Bauabnahme</b>. Dieser Abnahmetermin ist für den Bauherrn besonders wichtig, da zum einen meist die letzte Rate für die Bauleistung fällig wird, zum anderen sich die Beweislast umkehrt und die Verjährungsfrist beginnt.</p>
+
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <img src="/img/baucampus-baubegleitung.jpg" class="img-fluid" alt="">
+          </div>
+        </div>
+
+      </div>
+</div><!-- End About Section -->
+<!-- END BLOG ENTRIES -->
+</div>
+
+<!-- END GRID -->
+</div><br>
+
+<!-- ======= About Section ======= -->
+<div id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Energieberatung {{$ortsname}}</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-right">
+          <p>Die Energieberatung durch einen zertifizierten Energieberater an ihrer Immobilie, soll ihnen einen Überblick über mögliche <b>Modernisierungsmaßnahmen</b> sowie <b>Fördermöglichkeiten</b> geben. Unsere Energieberater beraten wertneutral und objektiv und zeigen ihnen auf welche Maßnahmen im Rahmen der Energieberatung durch einen Energieberater sinnvoll sind und welche zu einem späteren Zeitpunkt durchgeführt werden können. </p>
+<p>Der Energieberater wird während der Energieberatung auf ihre Immobilie eingehen und sich ebenfalls mit den Gegebenheiten ihrer Immobilie auseinandersetzen.</p> 
+<p>Da die meisten Menschen weniger über den <b>Energiebedarf von Immobilien</b> wissen, als bei anderen Gegenständen des täglichen Lebens wie z.B. Autos oder Haushaltsgeräten hat sich der Gesetzgeber dafür entschieden, dass für den Verkauf und die Vermietung von Gebäuden gültige Energieausweise gem. ENEV vorzulegen sind. Erschwert wird die Einschätzung häufig noch für Privatpersonen, dadurch das allgemeingültige Informationen über den Energieausweis Mangelware sind und Vergleichsobjekte fehlen. Der Energieberater kann ihnen sowohl bei der Erstellung des Energieausweises für ihre Immobilie helfen, als auch bei dem Kauf einer Immobilie eine <b>energetische Einschätzung</b> geben.</p> 
+<p>Es gibt zwei verschieden Arten von Energieausweisen zum einen gibt es den <b>Verbrauchsausweis</b>, welcher auf von Kunden gelieferten Informationen bezüglich des Energieverbrauch der letzten drei Jahre basiert. Zum anderen gibt es den Bedarfsausweis. Der <b>Bedarfsausweis</b> wird verwendet wenn es keine bisherigen Werte gibt, die als Grundlage dienen können. Hier wird zur Berechnung die Bauform, das Alter der Immobilie, die Heizung als auch die Dämmung verwendet.</p>
+ 
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <img src="/img/baucampus-energieberatung.jpg" class="img-fluid" alt="">
+          </div>
+        </div>
+
+      </div>
+</div><!-- End About Section -->
+<!-- END BLOG ENTRIES -->
+</div>
+
+<!-- END GRID -->
+</div><br>
+ <!-- ======= About Section ======= -->
+ <div id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Schimmelpilz {{$ortsname}}</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-right">
+          <p>Schimmel oder eher gesagt Schimmelpilz in einem Haus oder einer Wohnung sind nicht nur eine optische Beeinträchtigung, sondern kann auch zu <b>gesundheitlichen Problemen</b> führen. Weiter schädigen Schimmelpilze die Bausubstanz ihres Gebäudes und führen zu einer <b>Wertminderung</b> der Immobilie (merkantiler Minderwert).</p>
+<p>Um den Schaden durch Schimmel möglichst klein zu halten, kann einem ein Bausachverständiger <b>unkompliziert und schnell</b> helfen. Der qualifizierte Bausachverständige nimmt die von Schimmel befallenen Stellen vor Ort in Augenschein, um die möglichen Ursachen zu ermitteln und die weitere Vorgehensweise mit ihnen abzustimmen. Durch die Zusammenarbeit mit dem Gutachter bei einem Befall durch Schimmelpilze wird in der Regel die <b>Schimmelpilzsanierung</b> vereinfacht. Durch das umfassende Wissen des Bausachverständigen werden teure und oft auch halbherzige Sanierungsmaßnahmen verhindert. Wichtige Erstmaßnahmen, wie z.B. das Verhindern der Sporenverbreitung der Schimmelpilze und der Einsatz der richtigen Fachbetriebe, helfen den vorhandenen Schaden so klein wie möglich zu halten und vollständig zu beheben. Auch nach einer erfolgten Sanierung des Schimmelpilzes helfen unsere Gutachter, den Erfolg der Maßnahmen zu überprüfen und festzuhalten. Des Weiteren steht der Bausachverständige ihnen mit Rat und Tat zur Seite, um auch in Zukunft Schimmel oder Schimmelpilz in ihrer Immobilie zu vermeiden, damit Sie oder ihre Mieter wieder beruhigt in ihren 4 Wänden leben können.</p>
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <img src="/img/baucampus-laptop.jpg" class="img-fluid" alt="">
+          </div>
+        </div>
+
+      </div>
+</div><!-- End About Section -->
+<!-- END BLOG ENTRIES -->
+</div>
+
+<!-- END GRID -->
+</div><br>
+<!-- ======= About Section ======= -->
+<div id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Bauschaden {{$ortsname}}</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-right">
+          <p>Die <b>baubegleitende Qualitätssicherung</b> durch einen Bausachverständigen ermöglicht dem Bauherren eine neutrale Qualitätskontrolle für den Bau oder den Umbau ihrer Immobilie. Durch die steigende Konkurrenz und Kostendruck in der Baubranche leidet die Bauqualität. Hier kann ein Bausachverständiger helfend eingreifen und so Geld und Nerven sparen. Ein Baugutachter, der die <b>baubegleitende Qualitätssicherung</b> durchführt, stellt sicher, dass Baubeschreibung, Bauplanung und Bauausführung übereinstimmen.</p>
+          <p>Die Ursachen von Baumängeln und Bauschäden sind, ohne kompetente und <b>fachliche Hilfe</b> durch einen Bausachverständigen nur schwer zu ermitteln. Ein Haus ist, im Vergleich mit der Serienfertigung, immer als ein <b>handwerkliches Einzelstück</b> zu sehen, dass durch die Vielzahl der Gewerke und Materialien, Planänderungen oder Planungsfehlern nicht fehlerfrei erstellt werden kann.</p>
+<p>So entstehen Baumängel, das heißt das Gebäude wurde nicht wie im Kaufvertrag oder Bauvertrag vereinbart hergestellt. Diese Baumängel führen oft zu Bauschäden, die nicht nur optische oder <b>funktionelle Beeinträchtigungen</b>, sondern sie können auch die Bausubstanz schädigen. Hierbei ist es einerlei, ob diese Bauschäden bei einem Neubau oder durch die Modernisierung einer Bestandsimmobilie entstanden sind.</p>
+<p>Als Bauherr und Kunde wollen Sie diese Mängel nicht hinnehmen. Ein Bausachverständiger ist hier der <b>kompetente Ansprechpartner</b>. Der Bausachverständige kann eine neutrale Ursachenermittlung für en Baumangel bzw. Bauschaden vornehmen und helfen, das Ausmaß des Schadens durch fachlich qualifizierte Erstmaßnahmen zu begrenzen. Das einschalten eines Gutachters und dessen <b>fachliche Erläuterungen</b> bzw. dessen Gutachten gibt ihnen als Bauherr die notwendige Sicherheit mit den Handwerkern und Bauunternehmern auf Augenhöhe zu verhandeln.</p>
+ 
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <img src="/img/baucampus-bauschaden.jpg" class="img-fluid" alt="">
+          </div>
+        </div>
+
+      </div>
+</div><!-- End About Section -->
+<!-- END BLOG ENTRIES -->
+</div>
+
+<!-- END GRID -->
+</div><br>
+@endif
 @endsection
 
