011d3878a0b6de1125be8509de13b91b0d9ec049
Oskar Lohse <oskar@baucampus.de>
2023-02-14 09:44:01 +0100

    test 2

--- resources/views/partials/_bundeslaender.blade.php
+++ resources/views/partials/_bundeslaender.blade.php
@@ -1,11 +1,11 @@
-@isset($domain)
+
     @foreach($regions as $region)
 
-@if ($region->Country == Austria)
+
     <a class="link-dark" href="/{{$region->Region}}/bausachverstaendiger">Baugutachter {{$region->Region}}</a>
-    @endif
+
   
     @endforeach
-@endisset
+
 
 
