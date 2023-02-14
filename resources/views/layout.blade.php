10deb77a19bbe2c9ef2f0e9593a37b9fb7ff313d
Oskar Lohse <oskar@baucampus.de>
2023-02-14 12:13:38 +0100

    unterseiten region

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -393,7 +393,7 @@ object-position: center;" class="description"><br>
                     
                     
 @else
-
+@if(str_contains(url()->current(), "bausachverstaendiger/"))
 
 <div id="services" class="services">
         <div class="container">
@@ -427,7 +427,42 @@ object-position: center;" class="description"><br>
         </div>
 <div class="container"><a class="description link-secondary mb-5" href="/impressum">Impressum</a>
                     <a class="description link-secondary mb-5" href="/datenschutzerklaerung">Datenschutzerklärung</a>
-  
+                    @else
+                    
+<div id="services" class="services">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
+                    
+                    <img src="/img/logo.png" alt="" style="    vertical-align: middle;
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    object-fit: cover;
+object-position: center;" class="description"><br>
+
+                </div>
+                <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
+                <p class="description">
+                    @include('partials._bundeslaender')
+                    </p>
+                </div>
+                <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
+                    
+                    <p class="description">
+
+@include('partials._orte')
+</p>
+                </div>
+                
+
+            </div>
+        </div>
+<div class="container"><a class="description link-secondary mb-5" href="/impressum">Impressum</a>
+                    <a class="description link-secondary mb-5" href="/datenschutzerklaerung">Datenschutzerklärung</a>
+  @endif
+
 @endif
 <!-- End Footer Section -->
         </main><!-- End #main -->
