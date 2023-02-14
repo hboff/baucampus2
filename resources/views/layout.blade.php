c3beb16040c5a68c67df376f62feec70ed61fa9e
Oskar Lohse <oskar@baucampus.de>
2023-02-14 12:44:16 +0100

    test partial

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -344,54 +344,7 @@
     @yield('content')
     @include('partials._contact')
     @if(str_contains(url()->current(), "bausachverstaendiger"))
-    
-    <div id="services" class="services">
-        <div class="container">
-
-            <div class="row">
-                <div class="col-lg-3 col-md-6 icon-box" data-aos="fade-up">
-                    
-                    <img src="/img/logo.png" alt="" style="    vertical-align: middle;
-    width: 150px;
-    height: 150px;
-    border-radius: 50%;
-    object-fit: cover;
-object-position: center;" class="description"><br>
-
-                </div>
-                <div class="col-lg-3 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
-                <p class="description">
-                    @include('partials._bundeslaender')
-                    </p>
-                </div>
-                <div class="col-lg-3 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
-                    
-                    <p class="description">
-@switch($footer)
-                    @case('2')
-@include('partials._1')
-@break
-@case('1')
-@include('partials._orte')
-@break
-@default
-@endswitch
-
-</p>
-                </div>
-                
-                <div class="col-lg-3 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
-                <p class="description">
-                @include('partials._1')
-                </p>
-                </div>
-
-            </div>
-        </div>
-        <div class="container"><a class="description link-secondary mb-5" href="/impressum">Impressum</a>
-                    <a class="description link-secondary mb-5" href="/datenschutzerklaerung">Datenschutzerklärung</a>
-                    
-                    
+    @include('partials._test')
 @endif
 @if(str_contains(url()->current(), "baugutachter"))
 
