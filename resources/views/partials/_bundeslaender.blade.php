e9f427fa266409cd8f19d8375dba15305375d32d
Oskar Lohse <oskar@baucampus.de>
2023-02-14 09:53:12 +0100

    regions 5

--- resources/views/partials/_bundeslaender.blade.php
+++ resources/views/partials/_bundeslaender.blade.php
@@ -1,12 +1,12 @@
 
     @foreach($regions as $region)
     @if(str_contains(url()->current(), "baucampus.de"))
-    @if ($region->Country == Germany)
+    @if ($region->Country == 'Germany')
     <a class="link-dark" href="/{{$region->Region}}/bausachverstaendiger">Baugutachter {{$region->Region}}</a>
     @endif
 @endif
 @if(str_contains(url()->current(), "baucampus.be"))
-@if ($region->Country == Austria)
+@if ($region->Country == 'Austria')
     <a class="link-dark" href="/{{$region->Region}}/bausachverstaendiger">Baugutachter {{$region->Region}}</a>
     @endif
     @endif
