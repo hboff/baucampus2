9eb9be896ee48bd16be19c83efa3902b5c983b10
Oskar Lohse <oskar@baucampus.de>
2023-02-14 11:12:22 +0100

    footer 6

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -346,7 +346,7 @@
     @include('partials._contact')
     @foreach($ortat as $ort)
     @if(str_contains(url()->current(), "{{$ort->ort}}"))
-    
+    @break
     <div id="services" class="services">
         <div class="container">
 
@@ -386,7 +386,8 @@ object-position: center;" class="description"><br>
         <div class="container"><a class="description link-secondary mb-5" href="/impressum">Impressum</a>
                     <a class="description link-secondary mb-5" href="/datenschutzerklaerung">Datenschutzerklärung</a>
 @else
-@endforeach
+
+
 <div id="services" class="services">
         <div class="container">
 
@@ -420,7 +421,7 @@ object-position: center;" class="description"><br>
 <div class="container"><a class="description link-secondary mb-5" href="/impressum">Impressum</a>
                     <a class="description link-secondary mb-5" href="/datenschutzerklaerung">Datenschutzerklärung</a>
 @endif
-
+@endforeach
 <!-- End Footer Section -->
         </main><!-- End #main -->
 
