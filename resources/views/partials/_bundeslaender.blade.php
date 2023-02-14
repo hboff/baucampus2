8b5ad6cf6b42163f504d207591beb7fd744987b0
Oskar Lohse <oskar@baucampus.de>
2023-02-14 09:42:10 +0100

    region test

--- resources/views/partials/_bundeslaender.blade.php
+++ resources/views/partials/_bundeslaender.blade.php
@@ -2,12 +2,12 @@
     @foreach($regions as $region)
     @if ($domain == immobilienbewertung-bielefeld.com)
     @if ($region->Country == Germany)
-    <a class="link-dark" href="/{{$region->region}}/bausachverstaendiger">Baugutachter {{$region->region}}</a>
+    <a class="link-dark" href="/{{$region->Region}}/bausachverstaendiger">Baugutachter {{$region->Region}}</a>
     @endif
 @endif
 @if ($domain == baucampus.be)
 @if ($region->Country == Austria)
-    <a class="link-dark" href="/{{$region->region}}/bausachverstaendiger">Baugutachter {{$region->region}}</a>
+    <a class="link-dark" href="/{{$region->Region}}/bausachverstaendiger">Baugutachter {{$region->Region}}</a>
     @endif
     @endif
     @endforeach
