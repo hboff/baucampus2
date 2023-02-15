12ba93458bc7619368fc8e810e4126c738d1478b
Oskar Lohse <oskar@baucampus.de>
2023-02-15 10:24:57 +0100

    test

--- resources/views/partials/_contact.blade.php
+++ resources/views/partials/_contact.blade.php
@@ -297,7 +297,8 @@
                                         @endif
                                     </div>  
                                 </div>
-                            </div>    
+                            </div>   
+                            <div class="row"> 
                             <label for="checkbox" style="font-size:200%">Zustimmung:</label>
                              <p><input type="checkbox"  name="checkbox" value="Checkbox" class="m-0 p-0"> 
                                          @if ($errors->has('checkbox'))
@@ -305,7 +306,8 @@
                                         @endif   
                                         
                               Ich stimme zu, dass meine Angaben aus dem Kontaktformular zur Bearbeitung meiner Anfrage erhoben und verarbeitet werden. Hinweis: Sie können Ihre Einwilligung jederzeit für die Zukunft per E-Mail widerrufen. Detaillierte Informationen zum Umgang mit Nutzerdaten finden Sie in unserer <a href="/datenschutzerklaerung">Datenschutzerklärung</a>.</p>
-                            <div class="form-group text-center mb-5">
+</div>
+                              <div class="form-group text-center mb-5">
                                             <div class="text-center"><button class="submit" style="    background-color: #149ddd; border: none; color: white; padding: 16px 32px; text-decoration: none; margin: 4px 2px; cursor: pointer; font-size: 16px; border-radius: 5px;">Senden</button></div>
 
                                 
