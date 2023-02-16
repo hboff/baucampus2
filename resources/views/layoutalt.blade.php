fd21397aaad7c9d06587e3335e382b170ffcc370
hboff <oskar@baucampus.de>
2023-02-16 12:17:26 +0100

    1

--- resources/views/layoutalt.blade.php
+++ resources/views/layoutalt.blade.php
@@ -22,7 +22,7 @@
     width: 50px;
     height: 50px;
     border-radius: 50%;
-  }
+  } 
 </style>
 
 <!DOCTYPE html>
@@ -32,7 +32,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Baucampus</title>
+  <title>Baucampus 1</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
