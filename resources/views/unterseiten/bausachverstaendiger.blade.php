66d825c1a0e42d23ce93d2ca58ba6bd6709eefcd
hboff <oskar@baucampus.de>
2023-02-16 13:09:51 +0100

    2

--- resources/views/unterseiten/bausachverstaendiger.blade.php
+++ resources/views/unterseiten/bausachverstaendiger.blade.php
@@ -14,29 +14,6 @@ $footer=1;
 </br>
 </br>
 
-<div id="about" class="about pt-5">
-      <div class="container">
-
-        <div class="section-title">
-          <h2>Hauskaufberatung {{$ortsname}}</h2>
-        </div>
-        <div class="row">
-          <div class="col-lg-6" data-aos="fade-right">
-          <p>Beim <b>Kauf eines Hauses</b> oder einer Eigentumswohnung stellen sich viele Fragen. Mit einer professionellen und unabhängigen <b>Hauskaufberatung</b> durch unsere Bausachverständigen gehen Sie den richtigen Schritt, um ein Haus oder eine Eigentumswohnung auf der Basis klarer <b>Fakten</b> zu erwerben.</p>
-
-<p>Im Vorfeld der Hauskaufberatung prüft ihr Bausachverständiger die <b>vorhandenen Unterlagen</b>, meist das Exposé und bereitet den Termin anhand der Eckdaten des Gebäudes durch eine <b>Marktwerteinschätzung</b> vor. Vor Ort begeht der Sachverständige mit Ihnen gemeinsam die Immobilie, die Sie erwerben möchten und stellt den <b>baulichen Zustand</b> und den eventuellen <b>Sanierungsbedarf</b> des Objektes fest. Zudem beantwortet der Baugutachter Fragestellungen, auf die es Ihnen besonders ankommt, z.B. bzgl. eines Umbaus.</p>
-<p>Im Anschluss an die Besichtigung der Immobilie findet eine <b>abschließende Besprechung</b> statt, bei der Ihnen ihr Bausachverständiger noch einmal alle Fakten, die Sie zum Kauf der Immobilie wissen müssen, verständlich und klar nachvollziehbar erklärt. Die Beratung durch unsere Bausachverständigen beim Kauf eines Hauses oder einer Eigentumswohnung gibt ihnen <b>Sicherheit</b>, so dass Sie ihre Kaufentscheidung beruhigt treffen können.</p>
-
-          </div>
-          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
-            <img src="/img/hauskaufberatung-brochure.jpg" class="img-fluid" alt="">
-          </div>
-        </div>
-
-      </div>
-</div><!-- End About Section -->
-<!-- END BLOG ENTRIES -->
-</div>
 
 <!-- END GRID -->
 </div><br>
@@ -170,7 +147,8 @@ $footer=1;
 
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
-            <img data-src="/img/hauskaufberatung-brochure.jpg" class="lazyload" alt="Alternative text">
+            <img data-src="/path/to/image.jpg" class="lazyload" alt="Alternative text">
+            <img src="/img/hauskaufberatung-brochure.jpg" class="img-fluid" alt="">
           </div>
         </div>
 
